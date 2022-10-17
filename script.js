018115adcbe8ae3ee3d20f4146a694094a4f20c8
João Souza <joaosouz8@gmail.com>
2022-10-16 22:03:33 -0300

    Primeiro commit do projeto

---
+++ script.js
@@ -0,0 +1,7 @@
+const burguer = document.querySelector('#burguer');
+const sidebar = document.querySelector('#side');
+burguer.addEventListener('click',function(){
+
+    if(sidebar.style.display =='none') sidebar.style.display = 'block';
+    else sidebar.style.display = 'none';
+});
