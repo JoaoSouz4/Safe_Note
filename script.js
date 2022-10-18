7532ed5828ddd1e8ada069187c049b68229c81b3
Joao Souza <jvcsouza12@gmail.com>
2022-10-17 23:52:01 -0300

    Adição de animação css

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 const burguer = document.querySelector('#burguer');
 const sidebar = document.querySelector('#side');
 burguer.addEventListener('click',function(){
-
-    if(sidebar.style.display =='none') sidebar.style.display = 'block';
-    else sidebar.style.display = 'none';
+    
+    if(sidebar.style.display ==='none') sidebar.style.display = 'block';
+    else{
+         sidebar.style.display = 'none'
+    } 
 });
+
