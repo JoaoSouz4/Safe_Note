af773f3577d92b0348b8bea28db56043e6b7559a
Joao Souza <jvcsouza12@gmail.com>
2022-10-20 05:57:06 -0300

    22/10/2022

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 let arrayClientela = [];
 let contador = 0;
 
-document.addEventListener('click', function(e){
+const btn = document.querySelector('#btn');
+const remove = document.querySelector('.pop');
+const spancontador = document.querySelector('#contador');
+
+
+btn.addEventListener('click', function(){
 
     const inputNome = document.querySelector('#nome');
     const inputIdade = document.querySelector('#idade');
@@ -9,7 +14,6 @@ document.addEventListener('click', function(e){
     const inputDia = document.querySelector('#dia');
     const inputHora = document.querySelector('#hora');
     const inputServico = document.querySelector('#servico');
-    const spancontador = document.querySelector('#contador');
 
     const nome = inputNome.value;
     const idade = inputIdade.value;
@@ -17,10 +21,6 @@ document.addEventListener('click', function(e){
     const dia = inputDia.value;
     const hora = inputHora.value;
     const servico = inputServico.value;
-
-    let el = e.target;
-
-    if(el.classList.contains('btn')){
 
         let Cliente = {
             nome : nome,
@@ -35,8 +35,63 @@ document.addEventListener('click', function(e){
         arrayClientela.push(Cliente);
         console.log(arrayClientela);
         alert('Cliente: '+Cliente.nome+' Registrado com sucesso!');
+
+        encontrarMaiorDia(arrayClientela);
+});
+
+remove.addEventListener('click', function(){
+    if(contador<=0) alert('Todos os clientes já foram removidos');
+    else{
+        contador--;
+        spancontador.innerHTML = contador;
+        arrayClientela.pop();
+        console.log(arrayClientela);
     }
 });
+
+function encontrarMaiorGenero(array){
+    let M = 0;
+    let F = 0;
+       for( let i = 0; i<array.length;i++){
+            if(array[i].sexo == 'Masculino') M++
+            else if (array[i].sexo == 'Feminino') F++
+       }
+       if(M>F){
+        console.log('A maioria são Homens: '+M)
+        return M
+       } 
+
+       else if(F>M){
+        console.log('A maioria são mulheres: '+F);
+       } 
+       else if(M==F){
+        console.log('Empate');
+        return F;
+       } 
+       
+}
+
+function encontrarMaiorDia(array){
+    let dias = {
+              segunda:0,terca:0,quarta:0,quinta:0,sexta:0,sabado:0
+        }
+
+        for(let i = 0;i<array.length;i++){
+            if(array[i].dia == 'Segunda-Feira') dias.segunda++;
+            else if(array[i].dia == 'Terça-Feira') dias.terca++;
+            else if(array[i].dia == 'Quarta-Feira') dias.quarta++;
+            else if(array[i].dia == 'Quinta-Feira') dias.quinta++;
+            else if(array[i].dia == 'Sexta-Feira') dias.sexta++;
+            else if(array[i].dia == 'Sábado') dias.sabado++;
+        }
+    let arraydias = Object.values(dias);
+   
+    
+}
+
+
+
+
 
 
 
